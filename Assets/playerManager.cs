9f9cca98a547b3d2b6163e0a50f31f49902284c6
Alessio Antonucci <61825057+iFralex@users.noreply.github.com>
2023-10-15 15:34:50 +0200

    Add files via upload

---
+++ Assets/playerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour
+{
+    public int nCubi, nBarre;
+    public Vita vitaSc;
+    public arma armaSc;
+    public inputManager imSc;
+    public movimento movSc;
+    public UIPlayer UIPSc;
+
+    void Start()
+    {
+        vitaSc = GetComponent<Vita>();
+        armaSc = GetComponent<arma>();
+        imSc = GetComponent<inputManager>();
+        movSc = GetComponent<movimento>();
+        UIPSc = GetComponent<UIPlayer>();
+    }
+
+    public void AcquistaBarraPerCubo()
+    {
+        if (nCubi > 0)
+        {
+            nCubi--;
+            nBarre++;
+            UIPSc.AggiornaValori(UIPSc.barreCont, nBarre);
+            UIPSc.AggiornaValori(UIPSc.cubiCont, nCubi);
+        }
+    }
+}
